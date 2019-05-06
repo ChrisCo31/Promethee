74aef9f0227b6d93648c1dcebce26976a982ed05
ChrisCo31 <christel.coeur@free.fr>
2019-05-06 14:31:55 +0200

    Menu Lateral V_2

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,20 +3,17 @@ import { Link } from 'gatsby'
 
 
 const Menu = (props) => (
-    <div className="navBar">
-       <button id="open-menu"><Link to="/"></Link></button>        
-           
-                <ul id="lateral-menu">
-                    <div id="block-menu">  
-                        <li class="menu-item draw" onclick="/"><Link to="/"><i class="icons">Home</i></Link></li>                
-                        <li class="menu-item draw" onclick="/ess"><Link to="/ess"><i class="icons">ESS ?</i></Link></li>
-                        <li class="menu-item draw" onclick="/prestations"><Link to="/prestations"><i class="icons">Services</i></Link></li>
-                        <li class="menu-item draw" onclick="/realisations"><Link to="/realisations"><i class="icons">Projets</i></Link></li>
-                        <li class="menu-item draw" onclick="/equipe"><Link to="/equipe"><i class="icons">Equipe</i></Link></li>
-                        <li class="menu-item draw" onclick="/blog"><Link to="/blog"><i class="icons">Blog</i></Link></li>
-                    </div>
-                </ul>
-            
+    <div className="test">
+       <div id='detection'></div>
+        <ul className='menu'>
+            <li id='home'><Link to="/"><i >Home</i></Link></li>
+            <li id='es'><Link to="/ess"><i>ESS ?</i></Link></li>
+            <li id='services'><Link to="/prestations"><i>Services</i></Link></li>
+            <li id='projets'><Link to="/realisations"><i>Projets</i></Link></li>
+            <li id='partenariats'><Link to="/partenariats"><i>Partenariats</i></Link></li>
+            <li id='equipe'><Link to="/equipe"><i>Equipe</i></Link></li>
+            <li id='blog'><Link to="/blog"><i>Blog</i></Link></li>
+        </ul>
 	</div>
 );
 export default Menu;
