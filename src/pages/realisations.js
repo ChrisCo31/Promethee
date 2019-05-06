e2c4688b60cac8cd052dfb64497e6456f33a5f79
ChrisCo31 <christel.coeur@free.fr>
2019-05-06 09:47:29 +0200

    Ebauche page réalisation

--- src/pages/realisations.js
+++ src/pages/realisations.js
@@ -6,6 +6,19 @@ class Realisations extends React.Component {
     render () {
         return (
             <Layout>
+                <section id="portfolio">
+                    <div className ="headProject">
+                        <h2> Les projets</h2>
+                    </div>
+                    <div className="BodyProject">
+                        <div className= "project">
+
+
+                        </div>
+                    </div>
+               </section>       
+                
+           
                   
             </Layout>
             
